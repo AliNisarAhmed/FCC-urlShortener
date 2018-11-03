fa2298c6f0ef11c73dc72cf2cd8a91925565c510
Glitch (hello-express) <none>
2018-11-03 05:24:22 +0000

    🚒🐗 Checkpoint ./server.js:2035816/120 ./views/index.html:2035816/39

--- server.js
+++ server.js
@@ -4,12 +4,15 @@
 // init project
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
